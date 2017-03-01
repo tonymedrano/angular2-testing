2afa71998a9bfb411999358aa66b64c8abadd78f
Tony Medrano <info@tonymedrano.com>
2017-03-01 15:49:16 +0100

    npm install & npm start & good to go!.

--- karma.conf.js
+++ karma.conf.js
@@ -1,78 +1,74 @@
-module.exports = function (config) {
-    config.set({
+// Karma configuration
+// Generated on Wed Mar 01 2017 15:46:01 GMT+0100 (Hora estándar romance)
 
-        // base path that will be used to resolve all patterns (eg. files, exclude)
-        basePath: './',
+module.exports = function(config) {
+  config.set({
 
-
-        // frameworks to use
-        // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-        frameworks: ['jasmine'],
+    // base path that will be used to resolve all patterns (eg. files, exclude)
+    basePath: '',
 
 
-        // for more information https://www.npmjs.com/package/karma-jspm
+    // frameworks to use
+    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
+    frameworks: ['jasmine'],
 
 
-        proxies: {
-            '/src/': '/base/src/'
-        },
+    // list of files / patterns to load in the browser
+    files: [
+      'src/app/**/*.spec.ts'
+    ],
+
+    // MimeType
+    mime: {
+      'text/x-typescript': ['ts','tsx']
+    },
 
 
-        // list of files to exclude
-        exclude: [],
+    // list of files to exclude
+    exclude: [
+    ],
 
 
-        // preprocess matching files before serving them to the browser
-        // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
-        preprocessors: {
-            'src/app/**/!(*.spec).js': ['coverage']
-        },
+    // preprocess matching files before serving them to the browser
+    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
+    preprocessors: {
+    },
 
 
-        // test results reporter to use
-        // possible values: 'dots', 'progress', 'mocha', 'coverage'
-        // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-        reporters: ['mocha', 'coverage'],
+    // test results reporter to use
+    // possible values: 'dots', 'progress'
+    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
+    reporters: ['progress'],
 
 
-
-        coverageReporter: {
-            includeAllSources: true,
-            reporters: [
-                { type: 'text-summary' }
-            ]
-        },
+    // web server port
+    port: 9876,
 
 
-        // web server port
-        port: 9876,
+    // enable / disable colors in the output (reporters and logs)
+    colors: true,
 
 
-        // enable / disable colors in the output (reporters and logs)
-        colors: true,
+    // level of logging
+    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+    logLevel: config.LOG_INFO,
 
 
-        // level of logging
-        // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-        logLevel: config.LOG_INFO,
+    // enable / disable watching file and executing tests whenever any file changes
+    autoWatch: true,
 
 
-        // enable / disable watching file and executing tests whenever any file changes
-        autoWatch: true,
+    // start these browsers
+    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+    browsers: ['Chrome'],
 
 
-        // start these browsers
-        // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-        browsers: ['PhantomJS2'], // 'Chrome'
+    // Continuous Integration mode
+    // if true, Karma captures browsers, runs the tests and exits
+    singleRun: false,
 
-
-        // Continuous Integration mode
-        // if true, Karma captures browsers, runs the tests and exits
-        singleRun: true,
-
-
-        // Concurrency level
-        // how many browser should be started simultanous
-        concurrency: Infinity
-    })
-};
+    // Concurrency level
+    // how many browser should be started simultaneous
+    concurrency: Infinity
+  })
+}
